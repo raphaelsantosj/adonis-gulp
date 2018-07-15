fa14b8aee3b6a1a03dab1da12f573fd87f7d20ff
Raphael Santos <raphaelsantos.j@gmail.com>
2018-07-15 16:50:25 -0300

    Compile all files ending with .scss

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function(){
-  return gulp.src('public/assets/scss/style.scss')
+  return gulp.src('public/assets/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
     .pipe(sass()) // Converts Sass to CSS with gulp-sass
     .pipe(gulp.dest('public/assets/css'))
 });
