a0063d6e68e852a884df5a4ef474daf8704a2ae1
Raphael Santos <raphaelsantos.j@gmail.com>
2018-07-15 17:41:46 -0300

    Minify sass files

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,53 @@
+/*
+ * Variables
+ */
+// Gulp require
 var gulp = require('gulp');
+
+// Gulp Sass require
 var sass = require('gulp-sass');
 
-gulp.task('sass', function(){
-  return gulp.src('public/assets/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
-    .pipe(sass()) // Converts Sass to CSS with gulp-sass
-    .pipe(gulp.dest('public/assets/css'))
+// Gulp rename require
+var rename = require('gulp-rename');
+
+// Scss Files
+var scss = 'public/assets/scss/**/*.scss';
+
+// CSS destination
+var dest = 'public/assets/css';
+
+// Options for development
+var sassDevOptions = {
+  output: 'expanded'
+}
+
+// Options for production
+var sassProdOptions = {
+  output: 'compressed'
+}
+
+/*
+ * Tasks
+ */
+// Task 'sassdev' - Run with command 'gulp sassdev'
+gulp.task('sassdev', function() {
+  return gulp.src(scss)
+    .pipe(sass(sassDevOptions).on('error', sass.logError))
+    .pipe(gulp.dest(dest));
 });
 
-gulp.task('watch', function(){
-  gulp.watch('public/assets/scss/**/*.scss', ['sass']);
-})
+// Task 'sassprod' - Run with command 'gulp sassprod'
+gulp.task('sassprod', function() {
+  return gulp.src(scss)
+    .pipe(sass(sassProdOptions).on('error', sass.logError))
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest(dest));
+});
+
+// Task 'watch' - Run with command 'gulp watch'
+gulp.task('watch', function() {
+  gulp.watch(scss, ['sassdev', 'sassprod']);
+});
+
+// Default task - Run with command 'gulp'
+gulp.task('default', ['sassdev', 'sassprod', 'watch']);
